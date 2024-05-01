2617b3ffa42bb276e54b1cf17ea35f975823129c
Rustamli510 <qedir.rustemli02@gmail.com>
2024-05-02 02:35:25 +0400

    first commit

---
+++ src/Context/Context.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+
+    const contextValue = {
+
+    }
+
+    return (
+        <ContextProvider value = {contextValue}>
+            {props.children}
+        </ContextProvider>
+    )
+}
+
+export default ContextProvider;
